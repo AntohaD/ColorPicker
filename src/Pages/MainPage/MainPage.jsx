be35d7363d6770721a5ef04b2e62d0b7c9184fcc
Anton Dzherdzh <antondzherdzh@gmail.com>
2020-01-17 23:19:33 +0200

    add link github

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -7,15 +7,20 @@ import './MainPage.scss';
 
 function MainPage() {
   return (
-    <Jumbotron className="main-jumb">
-      <h3 className="main-jumb__title">Color Picker</h3>
-      <ColorForm 
-        value={'#A9A9A9'}
-        onChange
-        colors={DataColors.colors}
-      />
-    </Jumbotron>
-  )
+    <>
+      <Jumbotron className="main-jumb">
+        <h3 className="main-jumb__title">Color Picker</h3>
+        <ColorForm value={"#A9A9A9"} onChange colors={DataColors.colors} />
+      </Jumbotron>
+      <a
+        className="link-github"
+        href="https://github.com/AntohaD/ColorPicker"
+        target="_blank"
+      >
+        Project on GitHub
+      </a>
+    </>
+  );
 }
 
 export default MainPage;
